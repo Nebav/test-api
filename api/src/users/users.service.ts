89a32f327f37cfecdf79672328e27623618690cb
Yahia <yahiabrahimi@gmail.com>
2019-01-17 15:01:31 +0100

    [feat] Now returning user upon creation

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -11,9 +11,15 @@ export class UsersService {
       db.run(
           "INSERT INTO users (id, username)" +
           "VALUES (?, ?)", [uuid.v1().replace(/-/g, ""), username], (err) => {
-              return !err ?
-                  resolve({'message':'User has been registered'}) :
-                  reject(new HttpException(err, 500))
+            if (!err) {
+              db.get("SELECT * FROM users WHERE username = ?", [username], (err, row) => {
+                return !err ?
+                    resolve(row) :
+                    reject(new HttpException(err, 500))
+              })
+            } else {
+              reject(new HttpException(err, 500))
+            }
       })
   })
   }
